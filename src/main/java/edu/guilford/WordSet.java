818efbd4f5c1b68a510b1467af35a162333e3ea4
cordaishadie <131273919+cordaishadie@users.noreply.github.com>
2023-05-08 15:15:20 -0400

    second commit

---
+++ src/main/java/edu/guilford/WordSet.java
@@ -0,0 +1,25 @@
+package edu.guilford;
+
+import java.util.Collections;
+
+public class WordSet implements Comparable<WordSet>
+{
+    // attributes
+    private String word;
+    private int count;
+
+    /* constructor
+    public WordSet(String word, int count) {
+        // for (int i = 0; i < sortedList.size(); i++) {
+            word = sortedList.get(i);
+            count = Collections.frequency(sortedList, sortedList.get(i)));
+        }
+    } */
+
+    @Override
+    public int compareTo(WordSet o) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
+    }
+    
+}
