50d142aacd8223c7f2d62cfef9e6e755a4140ae1
cordaishadie <131273919+cordaishadie@users.noreply.github.com>
2023-05-08 15:18:18 -0400

    beginning WordSet class

--- src/main/java/edu/guilford/DataStructuresProject.java
+++ src/main/java/edu/guilford/DataStructuresProject.java
@@ -15,6 +15,8 @@ import java.util.InputMismatchException;
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Hello world!
@@ -27,6 +29,7 @@ public class DataStructuresProject {
         Path dataLocation = null;
         boolean fileExists = false;
         LinkedList sortedList = null;
+        TreeSet set = null;
         // String fileName = null;
 
         // In this part of the program, I wil create a code that sorts the words in the
@@ -61,33 +64,57 @@ public class DataStructuresProject {
             System.exit(1);
         }
 
-        // print the linkedlist
+        // In this part of the program, I will a set that contains the words in the
+        // LinkedList and the number of times each word appears in the LinkedList.
+        set = new TreeSet();
+        // add the words in the LinkedList to the set.
         for (int i = 0; i < sortedList.size(); i++) {
-            System.out.println(sortedList.get(i));
+            set.add(sortedList.get(i) + " " + Collections.frequency(sortedList, sortedList.get(i)));
         }
-        // Step 5: Sort the words in the Shadie_C_ResearchPaper.txt file alphabetically
 
+        // sort the set by the number of times each word appears
+        
     }
 
     // method that returns the number of words in the Shadie_C_ResearchPaper.txt
     // file.
 
-    public static LinkedList readData(Scanner scan) {
+    public static LinkedList readData(Scanner scan) throws NoSuchElementException, IndexOutOfBoundsException {
         // read the words from the Shadie_C_ResearchPaper.txt file.
         LinkedList inputWords = new LinkedList();
         String word = scan.next();
         // seperate the words using the space character.
-
         try {
-            for (int i = 0; i < 5000; i++) {
-                if (scan.hasNext()) {
+                while (scan.hasNext()) {
                     word = scan.next();
-                    inputWords.add(word);
+                    // check if the word is a number using the isNumber method.
+                    if (isNumber(word) == true) {
+                        // go to the next word.
+                        continue;
+                    }
+                    // check the last character of the word to see if it is a letter.
+                    while (isPunctuation(word.charAt(word.length() - 1)) == true) {
+                        // delete the first character of the word.
+                        word = word.substring(0, word.length() - 1);
+                    }
+                    // check the first character of the word to see if it is a letter.
+                    while (isPunctuation(word.charAt(0)) == true) {
+                        // delete the last character of the word.
+                        word = word.substring(1, word.length());
 
+                    }
+                
+                // if the word is a word and not a number, add it to the list.
+                // but if the word is a number, do not add it to the list.
+                if (isNumber(word) == true) {
+                    continue;
+                } else {
+                    word = word.toLowerCase();
+                    inputWords.add(word);
                 }
             }
-        } catch (NoSuchElementException ex) {
-            System.out.println("There are no more words in the file.");
+        } catch (NoSuchElementException | NumberFormatException | IndexOutOfBoundsException ex) {
+            ex.printStackTrace();
         }
         return inputWords;
     }
@@ -103,6 +130,31 @@ public class DataStructuresProject {
             bufferWrite.write(sortedList.get(i).toString());
         }
         bufferWrite.close();
+    }
+
+    // check if a string is a number using the Integer.parseInt method.
+    public static boolean isNumber(String word) {
+        try {
+            Integer.parseInt(word);
+            return true;
+        } catch (NumberFormatException ex) {
+            return false;
+        }
+    }
+
+    // check if character is a puntuation mark.
+    public static boolean isPunctuation(char character) {
+        if (character == '.' || character == ',' || character == '?' || character == '!' || character == ':'
+                || character == ';' || character == '(' || character == ')' || character == '[' || character == ']'
+                || character == '{' || character == '}' || character == '"' || character == '\'' || character == '-'
+                || character == '_' || character == '/' || character == '\\' || character == '<' || character == '>'
+                || character == '`' || character == '~' || character == '@' || character == '#' || character == '$'
+                || character == '%' || character == '^' || character == '&' || character == '*' || character == '+'
+                || character == '‘' || character == '’' || character == '“' || character == '”' || character == '’') {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
